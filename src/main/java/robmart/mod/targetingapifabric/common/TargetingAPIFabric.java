b089e8d9672b4377d2fd117515df0dc1d65a2ab9
Robmart <robmart@users.noreply.github.com>
2023-07-09 01:12:22 +0200

    Remove debug changes

--- src/main/java/robmart/mod/targetingapifabric/common/TargetingAPIFabric.java
+++ src/main/java/robmart/mod/targetingapifabric/common/TargetingAPIFabric.java
@@ -33,23 +33,6 @@ public class TargetingAPIFabric implements ModInitializer {
             animals.addMemberClass(SquidEntity.class);
             animals.addMemberClass(MooshroomEntity.class);
             Targeting.registerFaction(animals);
-            Faction animals2 = new Faction("FarmAnimals2", true);
-            animals2.addFriendClass(PlayerEntity.class);
-            animals2.addMemberClass(CowEntity.class);
-            animals2.addMemberClass(SheepEntity.class);
-            animals2.addMemberClass(ChickenEntity.class);
-            animals2.addMemberClass(HorseEntity.class);
-            animals2.addMemberClass(LlamaEntity.class);
-            animals2.addMemberClass(DonkeyEntity.class);
-            animals2.addMemberClass(MuleEntity.class);
-            animals2.addMemberClass(PigEntity.class);
-            animals2.addMemberClass(ParrotEntity.class);
-            animals2.addMemberClass(RabbitEntity.class);
-            animals2.addMemberClass(OcelotEntity.class);
-            animals2.addMemberClass(WolfEntity.class);
-            animals2.addMemberClass(SquidEntity.class);
-            animals2.addMemberClass(MooshroomEntity.class);
-            Targeting.registerFaction(animals2);
         }));
 
         ServerLifecycleEvents.SERVER_STOPPED.register((server -> {
